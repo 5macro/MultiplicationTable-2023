86a4a393d544124cc3fee7c70adb395f15512b6f
Hong Wu <hong.wu@rea-group.com>
2023-10-31 16:04:49 +0800

    feat: implement the functionality of generting multiplication table

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,49 @@
+export type Multiplication = [number, number, number]
+
 export class MultiplicationTable {
-  public render(start: number, end: number): string{
-    return '1*1=1'
+  public renderMultiplicationTable(start: number, end: number): string|null {
+    if(!this.isValid(start, end)) {
+      return null
+    }
+    const table: Multiplication[][] = this.generateMultiplicationTable(start, end)
+    const tableString: string = this.render(table)
+    return tableString
+  }
+
+  private isValid(start: number, end: number): boolean {
+    return Number.isInteger(start) && Number.isInteger(end)
+        && this.isValidNumber(start) && this.isValidNumber(end)
+        && this.isEndGreaterOrEqualThanStart(start, end)
+  }
+
+  private isValidNumber(num: number): boolean {
+    return num>=1 && num<=1000
+  }
+  private isEndGreaterOrEqualThanStart(start: number, end: number): boolean {
+    return start <= end
+  }
+
+  private generateMultiplicationTable(start: number, end: number): Multiplication[][] {
+    const result: Multiplication[][] = []
+    for(let row = start; row<=end; row++) {
+      const line = this.generateMultiplicationRow(start, row)
+      result.push(line)
+    }
+    return result
+  }
+
+  private generateMultiplicationRow(start: number, row: number): Multiplication[]{
+    const line: Multiplication[] = []
+    for(let column=start; column <= row; column++) {
+      line.push([column, row, column*row])
+    }
+    return line
+  }
+
+
+  private render(table: Multiplication[][]): string {
+    return table.map(
+      row => row.map(equ => `${equ[0]}*${equ[1]}=${equ[2]}`).join('  ')
+    ).join('\n')
   }
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -8,9 +8,63 @@ describe('Multiplication Table', () => {
     const end = 1
 
     //when
-    const rendered = table.render(start, end)
+    const rendered = table.renderMultiplicationTable(start, end)
 
     //then
     expect(rendered).toBe('1*1=1')
+  })
+
+  it('should return null when the start less then 1', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 0
+    const end = 100
+
+    //when
+    const rendered = table.renderMultiplicationTable(start, end)
+
+    //then
+    expect(rendered).toBeNull()
+  })
+
+  it('should return null when the start greater then 1000', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 2000
+    const end = 100
+
+    //when
+    const rendered = table.renderMultiplicationTable(start, end)
+
+    //then
+    expect(rendered).toBeNull()
+  })
+
+  it('should return null when the start greater then end', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 1
+
+    //when
+    const rendered = table.renderMultiplicationTable(start, end)
+
+    //then
+    expect(rendered).toBeNull()
+  })
+
+  it('should render multiplication table of (2, 4)', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 4
+
+    // when
+    const rendered = table.renderMultiplicationTable(start, end)
+
+    // then
+    expect(rendered).toBe(`2*2=4
+2*3=6  3*3=9
+2*4=8  3*4=12  4*4=16`)
   })
 })
